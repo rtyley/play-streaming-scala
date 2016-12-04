77572a8c69ff1181120af211897a0bf4958f99c2
Roberto Tyley <roberto.tyley@gmail.com>
2016-12-04 22:46:32 +0000

    Frequently failing actor-ref source for Server-Sent Events
    What I'm trying to achieve:
    
    * a 'writable' `Source[String, _]` that I can push data into arbitrarily at
      a later date. Seems like that's what `Source.actorRef()` is for?
      `/scala/eventSource/kick` makes the actor send a message into the source.
    * a `Publisher` for which `fanout` is true (ie
      `Sink.asPublisher(fanout = true)`), so that multiple clients can all
      receive the resulting server-sent events.
    
    When I try this, I sometimes get success - everything works as intended,
    but more often than not, I get this error:
    
    ```
    akka.stream.impl.ActorPublisher$NormalShutdownException: Cannot subscribe to shut-down Publisher
    [error] a.s.Materializer - [actorPublisher] Upstream failed.
    ```

--- app/controllers/ScalaEventSourceController.scala
+++ app/controllers/ScalaEventSourceController.scala
@@ -3,21 +3,44 @@
  */
 package controllers
 
-import javax.inject.Singleton
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import javax.inject.{Inject, Singleton}
 
+import akka.actor._
+import akka.actor.{ActorRef, ActorSystem}
+import akka.stream.{Materializer, OverflowStrategy}
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import org.reactivestreams.Publisher
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
-class ScalaEventSourceController extends Controller with ScalaTicker {
+class ScalaEventSourceController @Inject()(
+  implicit actorSystem: ActorSystem,
+  mat: Materializer,
+  ec: ExecutionContext) extends Controller with ScalaTicker {
 
   def index() = Action {
     Ok(views.html.scalaeventsource())
   }
 
+  val (actorRef, publisher) =
+    Source.actorRef[String](1000, OverflowStrategy.dropHead).toMat(Sink.asPublisher(fanout = true))(Keep.both).run()
+
+  val sourceFromPublisher = Source.fromPublisher(publisher).log("actorPublisher")
+
   def streamClock() = Action {
-    Ok.chunked(stringSource via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+    Ok.chunked(sourceFromPublisher via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+  }
+
+  def kickRandomTime() = Action {
+    val s = DateTimeFormatter.ofPattern("HH mm ss").format(ZonedDateTime.now().minusSeconds(scala.util.Random.nextInt))
+    actorRef ! s
+    Ok(s"We sent '$s'")
   }
 
 }
