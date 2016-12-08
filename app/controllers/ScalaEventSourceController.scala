44fee4b294504deb7e2e59a8079cededa51f3582
Roberto Tyley <roberto.tyley@gmail.com>
2016-12-08 10:05:03 +0000

    Send events to SSE when you feel like it!
    http://localhost:9000/scala/eventSource/kick posts a string which is
    received by all people viewing http://localhost:9000/scala/eventSource
    
    See also https://github.com/playframework/play-streaming-scala/issues/11

--- app/controllers/ScalaEventSourceController.scala
+++ app/controllers/ScalaEventSourceController.scala
@@ -3,21 +3,44 @@
  */
 package controllers
 
-import javax.inject.Singleton
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import javax.inject.{Inject, Singleton}
 
+import akka.actor.ActorSystem
+import akka.stream.Materializer
+import akka.stream.scaladsl.{BroadcastHub, Keep, MergeHub, Source}
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
-class ScalaEventSourceController extends Controller with ScalaTicker {
+class ScalaEventSourceController @Inject()(
+  implicit actorSystem: ActorSystem,
+  mat: Materializer,
+  ec: ExecutionContext) extends Controller with ScalaTicker {
 
   def index() = Action {
     Ok(views.html.scalaeventsource())
   }
 
+  // Obtain a Sink and Source which will publish and receive from the "bus" respectively.
+  val (sink, source) =
+    MergeHub.source[String](perProducerBufferSize = 16)
+      .toMat(BroadcastHub.sink(bufferSize = 256))(Keep.both)
+      .run()
+
   def streamClock() = Action {
-    Ok.chunked(stringSource via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+    Ok.chunked(source via EventSource.flow).as(ContentTypes.EVENT_STREAM)
+  }
+
+  def kickRandomTime() = Action {
+    val s = DateTimeFormatter.ofPattern("HH mm ss").format(ZonedDateTime.now().minusSeconds(scala.util.Random.nextInt))
+
+    Source.single(s).runWith(sink)
+    Ok(s"We sent '$s'")
   }
 
 }
